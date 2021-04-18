a4579828a96de4e7ffc11f96bc26eedfb0f196a0
Marcin Mizgała <maro181114@gmail.com>
2021-04-18 19:29:17 +0200

    Add loggedInStatus and current logged user object in App's state and pass loggedInStatus to Authentication and Dashboard

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,13 +4,33 @@ import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Authentication from "./Authentication";
 import Dashboard from "./Dashboard";
 export default class App extends Component{
+    constructor() {
+        super();
+
+        this.state = {
+            loggedInStatus: "NOT_LOGGED_IN",
+            user: {}
+        }
+    }
   render() {
       return (
-          <div className="App">
+          <div className="App App-content">
               <BrowserRouter>
                   <Switch>
-                      <Route exact path={"/"} component={Authentication}/>
-                      <Route exact path={"/dashboard"} component={Dashboard}/>
+                      <Route
+                          exact
+                          path={"/"}
+                          render={props => (
+                              <Authentication {...props} loggedInStatus={this.state.loggedInStatus}/>
+                          )}
+                      />
+                      <Route
+                          exact
+                          path={"/dashboard"}
+                          render={props => (
+                              <Dashboard {...props} loggedInStatus={this.state.loggedInStatus}/>
+                          )}
+                      />
                   </Switch>
               </BrowserRouter>
           </div>
