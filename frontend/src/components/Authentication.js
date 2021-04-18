62c3f905839e8fcd46981c18fcf9b0e7211e59c5
Marcin Mizgała <maro181114@gmail.com>
2021-04-18 19:02:33 +0200

    Create authentication card with registration form and make handleChange function to change field states

--- frontend/src/components/Authentication.js
+++ frontend/src/components/Authentication.js
@@ -23,16 +23,71 @@ export default class Authentication extends Component {
     }
 
     handleChange(event) {
-        console.log("handle change", event);
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     }
 
     render() {
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label for id="email">E-mail</label>
-                    <input id="email" type="email" name="email" value={this.state.email} onChange={this.handleChange} required/>
-                </form>
+            <div className="container">
+                <div className="card">
+                    <form className="form" onSubmit={this.handleSubmit}>
+
+                        <label htmlFor="name">Name</label>
+                        <input
+                            className="input_field"
+                            id="name"
+                            type="text"
+                            name="name"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                            required/>
+
+                        <label htmlFor="surname">Surname</label>
+                        <input
+                            className="input_field"
+                            id="surname"
+                            type="text"
+                            name="surname"
+                            value={this.state.surname}
+                            onChange={this.handleChange}
+                            required/>
+
+                        <label htmlFor="email">E-mail</label>
+                        <input
+                            className="input_field"
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            required/>
+
+                        <label htmlFor="password">Password</label>
+                        <input
+                            className="input_field"
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            required/>
+
+                        <label htmlFor="passwordConfirmation">Password Confirmation</label>
+                        <input
+                            className="input_field"
+                            id="passwordConfirmation"
+                            type="password"
+                            name="passwordConfirmation"
+                            value={this.state.passwordConfirmation}
+                            onChange={this.handleChange}
+                            required/>
+
+                        <button className="form_button" type="submit">Register</button>
+
+                    </form>
+                </div>
             </div>
         );
     }
