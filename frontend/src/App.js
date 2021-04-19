23b117d3fb26c3e0a427acc422f84875473b0d70
Marcin Mizgała <maro181114@gmail.com>
2021-04-19 20:42:36 +0200

    Move App and UserDashboard to src directory, move Authentication to pages directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component}  from 'react';
-import '../App.css';
+import './App.css';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Authentication from "./Authentication";
 import UserDashboard from "./UserDashboard";
