0d0436d644de97e6f6f5583c4a9ea2c486e608aa
Marcin Mizgała <maro181114@gmail.com>
2021-04-22 22:14:59 +0200

    Display Authentication or UserDashboard depending on loggedInStatus

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,31 @@
 import React, {Component}  from 'react';
 import './App.css';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
-import Authentication from "./Authentication";
+import Authentication from "./pages/Authentication";
 import UserDashboard from "./UserDashboard";
+
 export default class App extends Component{
     constructor() {
         super();
 
         this.state = {
-            loggedInStatus: "NOT_LOGGED_IN",
+            loggedInStatus: "LOGGED_IN",
             user: {}
         }
     }
+
   render() {
-      return (
-          <div className="App App-content">
-              <BrowserRouter>
-                  <Switch>
-                      <Route
-                          exact
-                          path={"/"}
-                          render={props => (
-                              <Authentication {...props} loggedInStatus={this.state.loggedInStatus}/>
-                          )}
-                      />
-                      <Route
-                          exact
-                          path={"/dashboard"}
-                          render={props => (
-                              <UserDashboard {...props} loggedInStatus={this.state.loggedInStatus}/>
-                          )}
-                      />
-                  </Switch>
-              </BrowserRouter>
-          </div>
-      );
+        if(this.state.loggedInStatus === "NOT_LOGGED_IN") {
+            return(
+                <div className="App App_content">
+                    <Authentication loggedInStatus={this.state.loggedInStatus}/>
+                </div>
+            );
+        } else {
+                return(
+                    <div className="App App_content">
+                        <UserDashboard props={this.props} loggedInStatus={this.state.loggedInStatus}/>
+                    </div>
+                    );
+        }
   }
 }
