d438fefa2ad0d147c7dff7a2ba8188cb1b5604a4
Marcin Mizgała <maro181114@gmail.com>
2021-07-10 12:42:04 +0200

    Add Category and Nutrient classes

---
+++ backend/src/main/java/agh/edu/pl/diet/entities/Category.java
@@ -0,0 +1,31 @@
+package agh.edu.pl.diet.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long categoryId;
+    private String categoryName;
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
---
+++ backend/src/main/java/agh/edu/pl/diet/entities/Nutrient.java
@@ -0,0 +1,45 @@
+package agh.edu.pl.diet.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Nutrient")
+public class Nutrient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long nutrientId;
+    private String nutrientName;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "nutrients")
+    private Set<Product> products = new HashSet<>();
+
+    public Long getNutrientId() {
+        return nutrientId;
+    }
+
+    public void setNutrientId(Long nutrientId) {
+        this.nutrientId = nutrientId;
+    }
+
+    public String getNutrientName() {
+        return nutrientName;
+    }
+
+    public void setNutrientName(String nutrientName) {
+        this.nutrientName = nutrientName;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+}
