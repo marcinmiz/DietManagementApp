d92819602829d9dc37a3761f494d86069d28e27b
Marcin Mizgała <maro181114@gmail.com>
2021-07-10 12:35:14 +0200

    Add CORS Configuration

--- backend/src/main/java/agh/edu/pl/diet/DietApplication.java
+++ backend/src/main/java/agh/edu/pl/diet/DietApplication.java
@@ -1,8 +1,13 @@
 package agh.edu.pl.diet;
 
+import agh.edu.pl.diet.entities.Product;
+import agh.edu.pl.diet.entities.Recipe;
+import agh.edu.pl.diet.entities.RecipeProduct;
 import agh.edu.pl.diet.entities.User;
 import agh.edu.pl.diet.entities.security.Role;
 import agh.edu.pl.diet.entities.security.UserRole;
+import agh.edu.pl.diet.repos.RecipeRepo;
+import agh.edu.pl.diet.services.ProductService;
 import agh.edu.pl.diet.utility.SecurityUtility;
 import agh.edu.pl.diet.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +23,10 @@ public class DietApplication implements CommandLineRunner {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private RecipeRepo recipeRepo;
+    @Autowired
+    private ProductService productService;
 
     public static void main(String[] args) {
         SpringApplication.run(DietApplication.class, args);
@@ -25,16 +34,48 @@ public class DietApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user1 = new User();
-        user1.setUsername("admin");
-        user1.setPassword(SecurityUtility.passwordEncoder().encode("admin"));
-        user1.setEmail("admin@gmail.com");
-        Set<UserRole> userRoles = new HashSet<>();
-        Role role1 = new Role();
-        role1.setRoleId(0);
-        role1.setName("ROLE_ADMIN");
-        userRoles.add(new UserRole(user1, role1));
-
-        userService.createUser(user1, userRoles);
+//        User user1 = new User();
+//        user1.setUsername("admin");
+//        user1.setPassword(SecurityUtility.passwordEncoder().encode("admin"));
+//        user1.setEmail("admin@gmail.com");
+//        Set<UserRole> userRoles = new HashSet<>();
+//        Role role1 = new Role();
+//        role1.setRoleId(0);
+//        role1.setName("ROLE_ADMIN");
+//        userRoles.add(new UserRole(user1, role1));
+//
+//        userService.createUser(user1, userRoles);
+//
+//        Recipe recipe1 = new Recipe();
+//        recipe1.setRecipeName("cheesecake");
+//        recipe1.setOwner(user1);
+//
+//        Product product1 = new Product();
+//        product1.setTitle("white cheese");
+//        product1.setCalories("34");
+//        product1.setCategory("Dairy");
+//        product1.setProtein("26");
+//        product1.setTotalCarbohydrate("12");
+//
+//        productService.save(product1);
+//
+//        Product product2 = new Product();
+//        product2.setTitle("egg");
+//        product2.setCalories("12");
+//        product2.setCategory("Dairy");
+//        product2.setProtein("43");
+//        product2.setTotalCarbohydrate("32");
+//
+//        productService.save(product2);
+////
+////        RecipeProduct recipeProduct1 = new RecipeProduct();
+////        recipeProduct1.setRecipeProductId(recipe1, product1);
+////        recipeProduct1.setQuantity(1000.0);
+////        recipeProduct1.setUnit("g");
+////
+////        recipe1.getRecipeProducts().add(recipeProduct1);
+////
+//         recipeRepo.save(recipe1);
+////
     }
 }
