7ffa441e7b1a49fd389469ef103de5e97c98ff18
Marcin Mizgała <maro181114@gmail.com>
2021-07-10 13:05:14 +0200

    Append get all products, get product and create new product RestController api

--- backend/src/main/java/agh/edu/pl/diet/controllers/ProductController.java
+++ backend/src/main/java/agh/edu/pl/diet/controllers/ProductController.java
@@ -1,14 +1,13 @@
 package agh.edu.pl.diet.controllers;
 
 import agh.edu.pl.diet.entities.Product;
+import agh.edu.pl.diet.entities.Recipe;
+import agh.edu.pl.diet.payloads.ProductRequest;
 import agh.edu.pl.diet.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,99 +18,114 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-@Controller
-@RequestMapping("/product")
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @RequestMapping(value = "/new/edit", method = RequestMethod.GET)
-    public String addProduct(Model model) {
-        Product product = new Product();
-        model.addAttribute("product", product);
-        return "new/edit";
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
     }
 
-    @RequestMapping(value = "/new/edit", method = RequestMethod.POST)
-    public String addProductPost(@ModelAttribute("product") Product product, HttpServletRequest request) {
-        productService.save(product);
-
-
-        MultipartFile prductImage = product.getProductImage();
-
-        try {
-            byte[] bytes = prductImage.getBytes();
-            String name = product.getId() + ".png";
-            BufferedOutputStream stream = new BufferedOutputStream(
-                    new FileOutputStream(new File("frontend/src/images/" + name)));
-            stream.write(bytes);
-            stream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return "redirect:products";
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable("id") Long product_id) {
+        return productService.getProduct(product_id);
     }
 
-    @RequestMapping("/productInfo")
-    public String productInfo(@RequestParam("id") Long id, Model model) {
-        Product product = productService.findOne(id);
-        model.addAttribute("product", product);
-
-        return "productInfo";
+    @PostMapping("/add")
+    public Product addNewProduct(@RequestBody ProductRequest productRequest) {
+        return productService.addNewProduct(productRequest);
     }
 
-    @RequestMapping("/updateProduct")
-    public String updateProduct(@RequestParam("id") Long id, Model model) {
-        Product product = productService.findOne(id);
-        model.addAttribute("product", product);
-
-        return "updateProduct";
-    }
-
-    @RequestMapping(value = "/updateProduct", method = RequestMethod.POST)
-    public String updateProductPost(@ModelAttribute("product") Product product, HttpServletRequest request) {
-        productService.save(product);
-
-        MultipartFile productImage = product.getProductImage();
-
-        if (!productImage.isEmpty()) {
-            try {
-                byte[] bytes = productImage.getBytes();
-                String name = product.getId() + ".png";
-
-                Files.delete(Paths.get("frontend/src/images/" + name));
-
-                BufferedOutputStream stream = new BufferedOutputStream(
-                        new FileOutputStream(new File("frontend/src/images/" + name)));
-                stream.write(bytes);
-                stream.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        return "redirect:/product/productInfo?id=" + product.getId();
-    }
-
-    @RequestMapping("/productList")
-    public String productList(Model model) {
-        List<Product> productList = productService.findAll();
-        model.addAttribute("productList", productList);
-        return "productList";
-
-    }
-
-    @RequestMapping(value = "/remove", method = RequestMethod.POST)
-    public String remove(
-            @ModelAttribute("id") String id, Model model
-    ) {
-        productService.removeOne(Long.parseLong(id.substring(8)));
-        List<Product> productList = productService.findAll();
-        model.addAttribute("productList", productList);
-
-        return "redirect:/product/productList";
-    }
+//    @RequestMapping(value = "/new/edit", method = RequestMethod.GET)
+//    public String addProduct(Model model) {
+//        Product product = new Product();
+//        model.addAttribute("product", product);
+//        return "new/edit";
+//    }
+//
+//    @RequestMapping(value = "/new/edit", method = RequestMethod.POST)
+//    public String addProductPost(@ModelAttribute("product") Product product, HttpServletRequest request) {
+//        productService.save(product);
+//
+//
+//        MultipartFile prductImage = product.getProductImage();
+//
+//        try {
+//            byte[] bytes = prductImage.getBytes();
+//            String name = product.getId() + ".png";
+//            BufferedOutputStream stream = new BufferedOutputStream(
+//                    new FileOutputStream(new File("frontend/src/images/" + name)));
+//            stream.write(bytes);
+//            stream.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return "redirect:products";
+//    }
+//
+//    @RequestMapping("/productInfo")
+//    public String productInfo(@RequestParam("id") Long id, Model model) {
+//        Product product = productService.findOne(id);
+//        model.addAttribute("product", product);
+//
+//        return "productInfo";
+//    }
+//
+//    @RequestMapping("/updateProduct")
+//    public String updateProduct(@RequestParam("id") Long id, Model model) {
+//        Product product = productService.findOne(id);
+//        model.addAttribute("product", product);
+//
+//        return "updateProduct";
+//    }
+//
+//    @RequestMapping(value = "/updateProduct", method = RequestMethod.POST)
+//    public String updateProductPost(@ModelAttribute("product") Product product, HttpServletRequest request) {
+//        productService.save(product);
+//
+//        MultipartFile productImage = product.getProductImage();
+//
+//        if (!productImage.isEmpty()) {
+//            try {
+//                byte[] bytes = productImage.getBytes();
+//                String name = product.getId() + ".png";
+//
+//                Files.delete(Paths.get("frontend/src/images/" + name));
+//
+//                BufferedOutputStream stream = new BufferedOutputStream(
+//                        new FileOutputStream(new File("frontend/src/images/" + name)));
+//                stream.write(bytes);
+//                stream.close();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        return "redirect:/product/productInfo?id=" + product.getId();
+//    }
+//
+//    @RequestMapping("/productList")
+//    public String productList(Model model) {
+//        List<Product> productList = productService.findAll();
+//        model.addAttribute("productList", productList);
+//        return "productList";
+//
+//    }
+//
+//    @RequestMapping(value = "/remove", method = RequestMethod.POST)
+//    public String remove(
+//            @ModelAttribute("id") String id, Model model
+//    ) {
+//        productService.removeOne(Long.parseLong(id.substring(8)));
+//        List<Product> productList = productService.findAll();
+//        model.addAttribute("productList", productList);
+//
+//        return "redirect:/product/productList";
+//    }
 
 }
